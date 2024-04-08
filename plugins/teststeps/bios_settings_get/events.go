15e43dc89ce770f45d54a57206ec4319c7808b05
Fabian Wienand <fabian.wienand@9elements.com>
2024-04-08 14:44:17 +0200

    refactor(teststep): bios settings get
    Signed-off-by: Fabian Wienand <fabian.wienand@9elements.com>

--- plugins/teststeps/bios_settings_get/events.go
+++ plugins/teststeps/bios_settings_get/events.go
@@ -50,33 +50,33 @@ func emitEvent(ctx xcontext.Context, name event.Name, payload interface{}, tgt *
 }
 
 // Function to format teststep information and append it to a string builder.
-func writeTestStep(step *TestStep, builders ...*strings.Builder) {
+func (ts TestStep) writeTestStep(builders ...*strings.Builder) {
 	for _, builder := range builders {
 		builder.WriteString("Input Parameter:\n")
 		builder.WriteString("  Transport:\n")
-		builder.WriteString(fmt.Sprintf("    Protocol: %s\n", step.Transport.Proto))
+		builder.WriteString(fmt.Sprintf("    Protocol: %s\n", ts.transport.Proto))
 		builder.WriteString("    Options: \n")
-		optionsJSON, err := json.MarshalIndent(step.Transport.Options, "", "    ")
+		optionsJSON, err := json.MarshalIndent(ts.transport.Options, "", "    ")
 		if err != nil {
-			builder.WriteString(fmt.Sprintf("%v", step.Transport.Options))
+			builder.WriteString(fmt.Sprintf("%v", ts.transport.Options))
 		} else {
 			builder.WriteString(string(optionsJSON))
 		}
 		builder.WriteString("\n")
 
 		builder.WriteString("  Parameter:\n")
-		builder.WriteString(fmt.Sprintf("    ToolPath: %s\n", step.Parameter.ToolPath))
+		builder.WriteString(fmt.Sprintf("    ToolPath: %s\n", ts.ToolPath))
 		builder.WriteString("\n")
 
 		builder.WriteString("  Options:\n")
-		builder.WriteString(fmt.Sprintf("    Timeout: %s\n", time.Duration(step.Options.Timeout)))
+		builder.WriteString(fmt.Sprintf("    Timeout: %s\n", time.Duration(ts.options.Timeout)))
 		builder.WriteString("\n")
 
 		builder.WriteString("Default Values:\n")
 		builder.WriteString(fmt.Sprintf("  Timeout: %s\n", defaultTimeout))
 
 		builder.WriteString("Expect Parameter:\n")
-		for i, expect := range step.expectStepParams {
+		for i, expect := range ts.Expect {
 			builder.WriteString(fmt.Sprintf("  Expect %d:\n", i+1))
 			builder.WriteString(fmt.Sprintf("    Option: %s\n", expect.Option))
 			builder.WriteString(fmt.Sprintf("    Value: %s\n", expect.Value))
