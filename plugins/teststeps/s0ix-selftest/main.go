4d45dcfcab769e0a032eb49058ab5fcbfa35cbb5
llogen <christoph.lange@9elements.com>
2024-04-09 09:21:27 +0200

    refactor(teststeps): s0ix-selftest
    Signed-off-by: llogen <christoph.lange@9elements.com>

--- plugins/teststeps/s0ix-selftest/main.go
+++ plugins/teststeps/s0ix-selftest/main.go
@@ -5,38 +5,27 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/insomniacslk/xjson"
 	"github.com/linuxboot/contest/pkg/event"
 	"github.com/linuxboot/contest/pkg/event/testevent"
 	"github.com/linuxboot/contest/pkg/test"
 	"github.com/linuxboot/contest/pkg/xcontext"
 	"github.com/linuxboot/contest/plugins/teststeps"
+	"github.com/linuxboot/contest/plugins/teststeps/abstraction/options"
+	"github.com/linuxboot/contest/plugins/teststeps/abstraction/transport"
 )
-
-// We need a default timeout to avoid endless running tests.
-const (
-	defaultTimeout = 10 * time.Minute
-	in             = "input"
-	exp            = "expect"
-)
-
-type inputStepParams struct {
-	Transport struct {
-		Proto   string          `json:"proto"`
-		Options json.RawMessage `json:"options,omitempty"`
-	} `json:"transport,omitempty"`
-
-	Options struct {
-		Timeout xjson.Duration `json:"timeout,omitempty"`
-	} `json:"options,omitempty"`
-}
 
 // Name is the name used to look this plugin up.
 var Name = "S0ix-Selftest"
 
+// We need a default timeout to avoid endless running tests.
+const (
+	defaultTimeout = 10 * time.Minute
+)
+
 // TestStep implementation for this teststep plugin
 type TestStep struct {
-	inputStepParams
+	transport transport.Parameters
+	options   options.Parameters
 }
 
 // Run executes the cmd step.
@@ -45,17 +34,21 @@ func (ts *TestStep) Run(ctx xcontext.Context, ch test.TestStepChannels, params t
 	return teststeps.ForEachTarget(Name, ctx, ch, tr.Run)
 }
 
-func (ts *TestStep) validateAndPopulate(stepParams test.TestStepParameters) error {
-	var (
-		input *test.Param
-	)
+func (ts *TestStep) populateParams(stepParams test.TestStepParameters) error {
+	var transportParams, optionsParams *test.Param
 
-	if input = stepParams.GetOne(in); input.IsEmpty() {
-		return fmt.Errorf("input parameter cannot be empty")
+	if transportParams = stepParams.GetOne(transport.Keyword); transportParams.IsEmpty() {
+		return fmt.Errorf("transport cannot be empty")
 	}
 
-	if err := json.Unmarshal(input.JSON(), &ts.inputStepParams); err != nil {
-		return fmt.Errorf("failed to deserialize %q parameters: %v", in, err)
+	if err := json.Unmarshal(transportParams.JSON(), &ts.transport); err != nil {
+		return fmt.Errorf("failed to deserialize transport: %v", err)
+	}
+
+	optionsParams = stepParams.GetOne(options.Keyword)
+
+	if err := json.Unmarshal(optionsParams.JSON(), &ts.options); err != nil {
+		return fmt.Errorf("failed to deserialize options: %v", err)
 	}
 
 	return nil
@@ -63,7 +56,7 @@ func (ts *TestStep) validateAndPopulate(stepParams test.TestStepParameters) erro
 
 // ValidateParameters validates the parameters associated to the TestStep
 func (ts *TestStep) ValidateParameters(_ xcontext.Context, params test.TestStepParameters) error {
-	return ts.validateAndPopulate(params)
+	return ts.populateParams(params)
 }
 
 // New initializes and returns a new HWaaS test step.
