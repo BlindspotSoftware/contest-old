2030e5a471c8c085e5ae92746db435d1b8593f33
llogen <christoph.lange@9elements.com>
2024-04-09 09:10:29 +0200

    refactor(teststeps): secureboot
    Signed-off-by: llogen <christoph.lange@9elements.com>

--- plugins/teststeps/secureboot/main.go
+++ plugins/teststeps/secureboot/main.go
@@ -5,59 +5,49 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/insomniacslk/xjson"
 	"github.com/linuxboot/contest/pkg/event"
 	"github.com/linuxboot/contest/pkg/event/testevent"
 	"github.com/linuxboot/contest/pkg/test"
 	"github.com/linuxboot/contest/pkg/xcontext"
 	"github.com/linuxboot/contest/plugins/teststeps"
+	"github.com/linuxboot/contest/plugins/teststeps/abstraction/options"
+	"github.com/linuxboot/contest/plugins/teststeps/abstraction/transport"
 )
 
+// Name is the name used to look this plugin up.
+var Name = "Secure Boot Management"
+
 const (
-	in  = "input"
-	out = "expect"
+	parametersKeyword = "parameters"
 )
 
 const (
 	defaultTimeout = time.Minute
 )
 
-type inputStepParams struct {
-	Transport struct {
-		Proto   string          `json:"proto"`
-		Options json.RawMessage `json:"options,omitempty"`
-	} `json:"transport"`
+type parameters struct {
+	Command         string `json:"command"`
+	ToolPath        string `json:"tool_path"`
+	Hierarchy       string `json:"hierarchy,omitempty"`
+	Append          bool   `json:"append,omitempty"`
+	KeyFile         string `json:"key_file,omitempty"`
+	CertFile        string `json:"cert_file,omitempty"`
+	SigningKeyFile  string `json:"signing_key_file,omitempty"`
+	SigningCertFile string `json:"signing_cert_file,omitempty"`
+	CustomKeyFile   string `json:"custom_key_file,omitempty"`
 
-	Options struct {
-		Timeout xjson.Duration `json:"timeout,omitempty"`
-	} `json:"options,omitempty"`
-
-	Parameter struct {
-		Command         string `json:"command"`
-		ToolPath        string `json:"tool_path"`
-		Hierarchy       string `json:"hierarchy,omitempty"`
-		Append          bool   `json:"append,omitempty"`
-		KeyFile         string `json:"key_file,omitempty"`
-		CertFile        string `json:"cert_file,omitempty"`
-		SigningKeyFile  string `json:"signing_key_file,omitempty"`
-		SigningCertFile string `json:"signing_cert_file,omitempty"`
-		CustomKeyFile   string `json:"custom_key_file,omitempty"`
-	} `json:"parameter"`
+	Expect struct {
+		SecureBoot bool `json:"secure_boot,omitempty"`
+		SetupMode  bool `json:"setup_mode,omitempty"`
+		ShouldFail bool `json:"should_fail,omitempty"`
+	} `json:"expect"`
 }
 
-type expect struct {
-	SecureBoot bool `json:"secure_boot,omitempty"`
-	SetupMode  bool `json:"setup_mode,omitempty"`
-	ShouldFail bool `json:"should_fail,omitempty"`
-}
-
-// Name is the name used to look this plugin up.
-var Name = "Secure Boot Management"
-
-// TestStep implementation for the exec plugin
+// TestStep implementation for this teststep plugin
 type TestStep struct {
-	inputStepParams
-	expect
+	parameters
+	transport transport.Parameters
+	options   options.Parameters
 }
 
 // Run executes the step.
@@ -66,43 +56,37 @@ func (ts *TestStep) Run(ctx xcontext.Context, ch test.TestStepChannels, params t
 	return teststeps.ForEachTarget(Name, ctx, ch, tr.Run)
 }
 
-func (ts *TestStep) validateAndPopulateParams(stepParams test.TestStepParameters) error {
-	var input *test.Param
+func (ts *TestStep) populateParams(stepParams test.TestStepParameters) error {
+	var parameters, transportParams, optionsParams *test.Param
 
-	if input = stepParams.GetOne(in); input.IsEmpty() {
-		return fmt.Errorf("input parameter cannot be empty")
+	if parameters = stepParams.GetOne(parametersKeyword); parameters.IsEmpty() {
+		return fmt.Errorf("parameters cannot be empty")
 	}
 
-	if err := json.Unmarshal(input.JSON(), &ts.inputStepParams); err != nil {
-		return fmt.Errorf("failed to deserialize %q parameters: %v", in, err)
+	if err := json.Unmarshal(parameters.JSON(), &ts.parameters); err != nil {
+		return fmt.Errorf("failed to deserialize parameters: %v", err)
 	}
 
-	if ts.Transport.Proto == "" {
-		return fmt.Errorf("transport protocol must not be empty")
+	if transportParams = stepParams.GetOne(transport.Keyword); transportParams.IsEmpty() {
+		return fmt.Errorf("transport cannot be empty")
 	}
 
-	if ts.Parameter.Command == "" {
-		return fmt.Errorf("command must not be empty")
+	if err := json.Unmarshal(transportParams.JSON(), &ts.transport); err != nil {
+		return fmt.Errorf("failed to deserialize transport: %v", err)
 	}
 
-	if ts.Parameter.ToolPath == "" {
-		return fmt.Errorf("ToolPath must not be empty")
-	}
+	optionsParams = stepParams.GetOne(options.Keyword)
 
-	expect := stepParams.GetOne(out)
-
-	if !expect.IsEmpty() {
-		if err := json.Unmarshal(expect.JSON(), &ts.expect); err != nil {
-			return fmt.Errorf("failed to deserialize %q parameters: %v", in, err)
-		}
+	if err := json.Unmarshal(optionsParams.JSON(), &ts.options); err != nil {
+		return fmt.Errorf("failed to deserialize options: %v", err)
 	}
 
 	return nil
 }
 
-// ValidateParameters validates the parameters associated to the step
-func (ts *TestStep) ValidateParameters(_ xcontext.Context, stepParams test.TestStepParameters) error {
-	return ts.validateAndPopulateParams(stepParams)
+// ValidateParameters validates the parameters associated to the TestStep
+func (ts *TestStep) ValidateParameters(ctx xcontext.Context, params test.TestStepParameters) error {
+	return ts.populateParams(params)
 }
 
 // New initializes and returns a new exec step.
